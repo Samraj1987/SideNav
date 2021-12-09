0561a379009c6cc8ee90d1a2babae418bd5239d9
Samraj <samraj.be@gmail.com>
2021-12-09 11:26:20 +0530

    Adding styles to Side nav for hover

--- sideNav/src/app/app.component.ts
+++ sideNav/src/app/app.component.ts
@@ -8,36 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'sideNav';
   isMenuHide: boolean = false;
-  showSubMenu: boolean = false;
-  //expand:string = '<i class="fa fa-plus-circle"></i>';
-  //collapse:string='<i class="fa fa-minus-circle"></i>';
-
-  //expand:string='<i class="fa fa-chevron-circle-down" aria-hidden="true"></i>';
-  //collapse:string='<i class="fa fa-chevron-circle-up" aria-hidden="true"></i>';
-
-  expand: string = '<i class="fa fa-caret-down" aria-hidden="true"></i>';
-  collapse: string = '<i class="fa fa-caret-up" aria-hidden="true"></i>';
-
-  //collapseIcon:string = '<i class="fa fa-angle-double-down" aria-hidden="true"></i>';
 
   moveSideMenu() {
     this.isMenuHide = !this.isMenuHide;
   }
 
-  switchToggleIcon(aElement: any) {
-    if (aElement.lastChild.lastChild.lastChild != null) {
-      (aElement.lastChild.lastChild.lastChild as HTMLElement).classList.toggle(
-        'fa-caret-up'
-      );
-      (aElement.lastChild.lastChild.lastChild as HTMLElement).classList.toggle(
-        'fa-caret-down'
-      );
-    }
-  }
-
   displaySubMenu(aElement: any) {
     (aElement as HTMLElement).classList.toggle('show');
-    this.switchToggleIcon(aElement);
   }
 
   showHideSubmenu(event: Event) {
@@ -45,6 +22,21 @@ export class AppComponent {
     var aElement = ele.closest('[data-dropdown-button]');
     if (aElement != null) {
       this.displaySubMenu(aElement);
+    }
+  }
+
+  showShrinkSubMenu(event: Event) {
+    var ele: HTMLElement = event.target as HTMLElement;
+    ele.classList.toggle('active', true);
+  }
+
+  hideShrinkSubMenu(event: Event) {
+    var ele: HTMLElement = event.target as HTMLElement;
+    var eleDropDownBtn = ele.previousElementSibling?.closest(
+      '[data-dropdown-button]'
+    );
+    if (eleDropDownBtn != null && eleDropDownBtn != undefined) {
+      eleDropDownBtn.classList.toggle('active', false);
     }
   }
 }
